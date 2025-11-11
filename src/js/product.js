cba1b9fc89ccf816f362258b271b8a77e4aef48d
baezbenjamin <baezbenjamin03@gmail.com>
2025-11-10 22:38:05 -0300

    Work Week 1 Individual

--- src/js/product.js
+++ src/js/product.js
@@ -4,11 +4,9 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  getLocalStorage(product);
+  setLocalStorage("so-cart", product);
 }
-
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
