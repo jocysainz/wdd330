d087a50f91651e6d62e95877993871df2d056d86
baezbenjamin <baezbenjamin03@gmail.com>
2025-11-19 01:31:18 -0300

    Remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,18 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  let deleteButtons = document.querySelectorAll('.btn');
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener('click', () => {
+      let cartItems = getLocalStorage("so-cart");
+      cartItems = cartItems.filter(item => item.Id !== deleteButtons[i].id);
+      setLocalStorage("so-cart", cartItems);
+      renderCartContents();
+    })
+  }
 }
 
 function cartItemTemplate(item) {
@@ -20,8 +29,9 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="btn" id="${item.Id}">❌</button>
 </li>`;
-
+  
   return newItem;
 }
 
