462d4be03b3bcafb7bb1103e6f1f2083dc4a6809
Testup476 <Kandajeanmarc@gmail.com>
2025-11-26 16:48:24 +0100

    Fix ProductData import path so build passes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import ProductData from "./ProductData.mjs";
+//import ProductData from "./ProductData.mjs";
 import ProducList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
